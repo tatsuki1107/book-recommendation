00084949af1d7366684b186bf98747a5ba6c529f
tatsuki1107 <tatsuki11070522@icloud.com>
2023-02-14 18:04:43 +0900

    rec api

--- api/main.py
+++ api/main.py
@@ -1,17 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
-from usa import get_recommend_list, get_history_list
+from usa import get_recommend_list, get_history_list, get_user_list
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.post('/api/recommend_books', response_model=List)
+
+@app.get('/api/recommend_books', response_model=List)
 def get_recommend_books(user_id: int):
     books = get_recommend_list(user_id)
     return books
 
 
-@app.post('/api/rating_history', response_model=List)
+@app.get('/api/rating_history', response_model=List)
 def get_rating_books(user_id: int):
     books = get_history_list(user_id)
     return books
+
+
+@app.post('/api/login')
+def login(user_id: int):
+    user_ids = get_user_list()
+    if not user_id in set(user_ids):
+        return HTTPException(status_code=404, detail="does not exist user_id")
+    return {"status": 200}
--- api/usa.py
+++ api/usa.py
@@ -1,6 +1,8 @@
 import joblib
+from fastapi import HTTPException
 from typing import List, Tuple
 import pandas as pd
+import numpy as np
 
 
 def _open_pkl() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
@@ -26,11 +28,11 @@ def get_recommend_list(user_id: int) -> List:
 
     user_df, book_df, rating_df = _open_pkl()
 
-    user_df = user_df[user_df["User-ID"] == user_id]
+    user_df = user_df[user_df["user_id"] == user_id]
     index = user_df.index[0]
     recommended_book = user_df["mf_recommend_item"][index]
 
-    book_df = book_df[book_df["ISBN"].isin(recommended_book)]
+    book_df = book_df[book_df["book_id"].isin(recommended_book)]
     book_info = _get_book_info(book_df)
 
     return book_info
@@ -40,11 +42,18 @@ def get_history_list(user_id: int) -> List:
 
     user_df, book_df, rating_df = _open_pkl()
 
-    rating_df = rating_df[rating_df["User-ID"] == user_id]
-    rating_df = rating_df.merge(book_df, on="ISBN")
+    rating_df = rating_df[rating_df["user_id"] == user_id]
+    rating_df = rating_df.merge(book_df, on="book_id")
 
-    rating_df = rating_df[rating_df.columns[rating_df.columns != 'User-ID']]
+    rating_df = rating_df[rating_df.columns[rating_df.columns != 'user_id']]
 
     book_info = _get_book_info(rating_df)
 
     return book_info
+
+
+def get_user_list() -> np.array:
+
+    user_df, book_df, rating_df = _open_pkl()
+
+    return user_df["user_id"].values
